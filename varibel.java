0a8d2910396f9c9b2d4292953b44d1203b4316a6
ziperkill <115127440+ziperkill@users.noreply.github.com>
2022-11-24 10:52:24 +0700

    Create varibel.java

---
+++ varibel.java
@@ -0,0 +1,12 @@
+public class Main
+{
+	public static void main(String[] args) {
+	    String salam = "assalamualaikum";
+String nama = "Arief selay";
+int lahir =  2003;
+
+System.out.println(salam);
+System.out.println("nama saya"+nama);
+System.out.println("saya lahir di tahun : "+lahir);
+	}
+}
